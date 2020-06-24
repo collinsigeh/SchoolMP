b9962bf6b02fe828a65e61740679344c6fee1d63
Collins Igeh <igehac@gmail.com>
2020-06-24 18:50:38 +0100

    Added labels to the registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,7 +16,9 @@
                         <input type="hidden" name="pic" value="default_userimage.png" />
                         
                         <div class="form-group row">
-                            <div class="col-md-8 offset-md-2">
+                            <label for="school" class="col-md-4 col-form-label text-md-right">{{ __('School') }}</label>
+
+                            <div class="col-md-6">
                                 <input id="school" type="text" class="form-control @error('school') is-invalid @enderror" name="school" value="{{ old('school') }}" required autocomplete="school" placeholder="Name of school" autofocus>
 
                                 @error('school')
@@ -28,7 +30,9 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-8 offset-md-2">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" placeholder="Your full name" autofocus>
 
                                 @error('name')
@@ -40,7 +44,9 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-8 offset-md-2">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
+
+                            <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Your email">
 
                                 @error('email')
@@ -52,7 +58,9 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-8 offset-md-2">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Your password">
 
                                 @error('password')
@@ -64,7 +72,9 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-8 offset-md-2">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm password') }}</label>
+
+                            <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirm password">
                             </div>
                         </div>
