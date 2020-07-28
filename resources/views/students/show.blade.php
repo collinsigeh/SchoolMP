f37e505117facfaf18fd3127b6c1af055af9180d
Collins Igeh <igehac@gmail.com>
2020-07-28 17:42:37 +0100

    student account subscription notice created in students.show view

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -49,6 +49,12 @@
         </div>
 
         <div class="welcome">
+            @if ($enrolment->status == 'Inactive')
+                <div class="alert alert-danger">
+                    <h4>Student Account Subscription Notice!</h4>
+                    The subscription for this student is <b>inactive</b>. <a href="#">Make payment</a> to activate.
+                </div>
+            @endif
 
             <div class="welcome">
                 <div class="row">
