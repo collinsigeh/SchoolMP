ebe95836a0ebcb7cf5825794067c2c99b244711c
Collins Igeh <igehac@gmail.com>
2020-08-03 10:05:13 +0100

    changed the headline for the student subscription notice in student_subscription_notice partial

--- resources/views/partials/_student_subscription_notice.blade.php
+++ resources/views/partials/_student_subscription_notice.blade.php
@@ -1,7 +1,7 @@
     @if($student_manager == 'Yes')
         @if ($enrolment->status == 'Inactive')
             <div class="alert alert-danger">
-                <h4>Student Account Subscription Notice!</h4>
+                <h4>Inactive Student Account!</h4>
                 The subscription for this student is <b>inactive</b>. <a href="#">Make payment</a> to activate.
             </div>
         @endif
