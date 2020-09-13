873ac2c2c42fa0e10d641c0150a15a3b73fc18ec
Collins Igeh <igehac@gmail.com>
2020-09-13 16:19:49 +0100

    updated the directors dashboard.show view

--- resources/views/directors/dashboard.blade.php
+++ resources/views/directors/dashboard.blade.php
@@ -59,104 +59,93 @@
           @endif
           <div class="row">
             <div class="col-md-12">
-                <div class="resource-details">
-                    <div class="title">
-                        Current Term
-                    </div>
-                    <div class="body">
-                      @if (!$currentterm)
-                          <span style="font-size: 3em;">Not Available!</span><br />
-                          @if (count($previousterms) >= 1)
-                            <a href="{{ route('terms.index') }}" class="btn btn-lg btn-primary" style="margin-top:- 15px; margin-right: 50px;">View session terms</a>
-                            <a href="{{ route('terms.create') }}" class="btn btn-lg btn-outline-primary" style="margin-top:- 15px;">New term</a> 
-                          @else
-                            <a href="{{ route('terms.create') }}" class="btn btn-lg btn-primary" style="margin-top:- 15px;">New term</a>
-                          @endif
-                      @else
-
-                      <span style="font-size: 3em;">{!! $currentterm->name.' (<small>'.$currentterm->session.'</small>)' !!}</span><br />
-                      <a href="{{ route('terms.show', $currentterm->id) }}" class="btn btn-lg btn-primary" style="margin-top:- 15px;">Enter</a>
-                      
-                      @endif
-                    </div>
+              <div class="alert alert-info">
+                <div>
+                  <img src="{{ config('app.url') }}/images/icons/current_term_icon.png" alt="current_term_icon" class="collins-current-term-icon">
                 </div>
+                
+                
+                @if (!$currentterm)
+                    <span style="font-size: 2em;">Not Available!</span><br />
+                    @if (count($previousterms) >= 1)
+                      <a href="{{ route('terms.index') }}" class="btn btn-sm btn-primary" style="margin-top:- 15px; margin-right: 50px;">View session terms</a>
+                      <a href="{{ route('terms.create') }}" class="btn btn-sm btn-outline-primary" style="margin-top:- 15px;">New term</a> 
+                    @else
+                      <a href="{{ route('terms.create') }}" class="btn btn-sm btn-primary" style="margin-top:- 15px;">New term</a>
+                    @endif
+                @else
+
+                <span style="font-size: 2em;">{!! $currentterm->name.' (<small>'.$currentterm->session.'</small>)' !!}</span><br />
+                <a href="{{ route('terms.show', $currentterm->id) }}" class="btn btn-sm btn-primary" style="margin-top:- 15px;">Enter</a>
+                
+                @endif
+              </div>
             </div>
           </div>
 
-          
           <div class="row">
-
-            <div class="col-md-6">
-              <div class="resource-details">
-                  <div class="title">
-                      School terms
-                  </div>
-                  <div class="body">
-                    @if (count($previousterms) < 1)
-                        None
-                    @else
-                    <div class="table-responsive">    
-                      <table class="table table-striped table-hover table-sm">
-                        <thead>
-                          <tr><th>#</th><th>School terms</th><th></th></tr>
-                        </thead>
-                        <tbody>
-                          @php
-                              $no_term_to_display = 5;
-                              $sn = 1;
-                              foreach ($previousterms as $sessionterm) {
-                                if($sn <= $no_term_to_display)
-                                {
-                                  echo '<tr><td>'.$sn.'</td><td>'.$sessionterm->name.' - <small>'.$sessionterm->session.'</small></td><td class="text-right"><a href="'.route('terms.show', $sessionterm->id).'" class="btn btn-sm btn-outline-primary">Enter</a></td></tr>';
-                                }
-                                $sn++;
-                              }
-                          @endphp
-                        </tbody>
-                      </table>
-                    </div>
-                    @if ($sn > $no_term_to_display)
-                        <a href="{{ route('terms.index') }}">View more...</a>
-                    @else
-                      <div class="text-right"><a class="btn btn-sm btn-primary" href="{{ route('terms.index') }}">View all</a></div>
-                    @endif
-                    @endif
-                  </div>
+            <div class="col-md-3">
+              <div class="collins-feature">
+                <a href="{{ route('terms.index') }}">
+                <img src="{{ config('app.url') }}/images/icons/terms_icon.png" alt="session_term" class="collins-feature-icon">
+                <div class="collins-feature-title">Session Terms</div>
+                </a>
               </div>
             </div>
-
-            <div class="col-md-6">
-              <div class="resource-details">
-                  <div class="title">
-                      More options
-                  </div>
-                  <div class="body">
-                    <div class="table-responsive">    
-                      <table class="table">
-                          <tr>
-                            <td><a class="btn btn-sm btn-block btn-outline-primary" href="{{ route('schools.edit', $school->id) }}">School information</a></td>
-                          </tr>
-                          <tr>
-                            <td><a class="btn btn-sm btn-block btn-outline-primary" href="{{ route('directors.index') }}">School directors</a></td>
-                          </tr>
-                          <tr>
-                            <td><a class="btn btn-sm btn-block btn-outline-primary" href="{{ route('staff.index') }}">School staff</a></td>
-                          </tr>
-                          <tr>
-                            <td><a class="btn btn-sm btn-block btn-outline-primary" href="{{ route('subjects.index') }}">School subjects</a></td>
-                          </tr>
-                          <tr>
-                            <td><a class="btn btn-sm btn-block btn-outline-primary" href="{{ route('classes.index') }}">School classes</a></td>
-                          </tr>
-                          <tr>
-                            <td><a class="btn btn-sm btn-block btn-outline-primary" href="{{ route('resulttemplates.index') }}">Result templates</a></td>
-                          </tr>
-                          <tr>
-                            <td><a class="btn btn-sm btn-block btn-outline-primary" href="{{ route('subscriptions.index') }}">Subscriptions and orders</a></td>
-                          </tr>
-                      </table>
-                    </div>
-                  </div>
+            <div class="col-md-3">
+              <div class="collins-feature">
+                <a href="{{ route('classes.index') }}">
+                <img src="{{ config('app.url') }}/images/icons/classes_icon.png" alt="classes_icon" class="collins-feature-icon">
+                <div class="collins-feature-title">Classes</div>
+                </a>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="collins-feature">
+                <a href="{{ route('subjects.index') }}">
+                <img src="{{ config('app.url') }}/images/icons/subjects_icon.png" alt="subjects_info" class="collins-feature-icon">
+                <div class="collins-feature-title">Subjects</div>
+                </a>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="collins-feature">
+                <a href="{{ route('resulttemplates.index') }}">
+                <img src="{{ config('app.url') }}/images/icons/result_template_icon.png" alt="result_template" class="collins-feature-icon">
+                <div class="collins-feature-title">Result Templates</div>
+                </a>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="collins-feature">
+                <a href="{{ route('directors.index') }}">
+                <img src="{{ config('app.url') }}/images/icons/director_icon.png" alt="director_icon" class="collins-feature-icon">
+                <div class="collins-feature-title">Directors</div>
+                </a>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="collins-feature">
+                <a href="{{ route('staff.index') }}">
+                <img src="{{ config('app.url') }}/images/icons/staff_icon.png" alt="staff_icon" class="collins-feature-icon">
+                <div class="collins-feature-title">Staff</div>
+                </a>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="collins-feature">
+                <a href="{{ route('schools.edit', $school->id) }}">
+                <img src="{{ config('app.url') }}/images/icons/school_info_icon.png" alt="schools_info" class="collins-feature-icon">
+                <div class="collins-feature-title">School Info</div>
+                </a>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="collins-feature">
+                <a href="{{ route('subscriptions.index') }}">
+                <img src="{{ config('app.url') }}/images/icons/subscription_icon.png" alt="subscription_icon" class="collins-feature-icon">
+                <div class="collins-feature-title">Subscriptions</div>
+                </a>
               </div>
             </div>
           </div>
