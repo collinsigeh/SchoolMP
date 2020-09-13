60ee271a0eacecba32257dfb9d412fd10da7c820
Collins Igeh <igehac@gmail.com>
2020-09-13 14:49:09 +0100

    schools.show view updated successfully

--- resources/views/schools/show.blade.php
+++ resources/views/schools/show.blade.php
@@ -37,9 +37,25 @@
           <div class="row">
             <div class="col-md-3">
               <div class="collins-feature">
-                <a href="{{ route('schools.edit', $school->id) }}">
-                <img src="{{ config('app.url') }}/images/icons/school_info_icon.png" alt="schools_info" class="collins-feature-icon">
-                <div class="collins-feature-title">School Info</div>
+                <a href="{{ route('terms.index') }}">
+                <img src="{{ config('app.url') }}/images/icons/terms_icon.png" alt="session_term" class="collins-feature-icon">
+                <div class="collins-feature-title">Session Terms</div>
+                </a>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="collins-feature">
+                <a href="{{ route('classes.index') }}">
+                <img src="{{ config('app.url') }}/images/icons/classes_icon.png" alt="classes_icon" class="collins-feature-icon">
+                <div class="collins-feature-title">Classes</div>
+                </a>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="collins-feature">
+                <a href="{{ route('subjects.index') }}">
+                <img src="{{ config('app.url') }}/images/icons/subjects_icon.png" alt="subjects_info" class="collins-feature-icon">
+                <div class="collins-feature-title">Subjects</div>
                 </a>
               </div>
             </div>
@@ -53,51 +69,37 @@
             </div>
             <div class="col-md-3">
               <div class="collins-feature">
-                <a href="{{ route('schools.edit', $school->id) }}">
-                <img src="{{ config('app.url') }}/images/icons/school_info_icon.png" alt="schools_info" class="collins-feature-icon">
-                <div class="collins-feature-title">Subjects</div>
+                <a href="{{ route('directors.index') }}">
+                <img src="{{ config('app.url') }}/images/icons/director_icon.png" alt="director_icon" class="collins-feature-icon">
+                <div class="collins-feature-title">Directors</div>
                 </a>
               </div>
             </div>
             <div class="col-md-3">
               <div class="collins-feature">
-                <a href="{{ route('resulttemplates.index') }}">
-                <img src="{{ config('app.url') }}/images/icons/result_template_icon.png" alt="result_template" class="collins-feature-icon">
-                <div class="collins-feature-title">Classes</div>
+                <a href="{{ route('staff.index') }}">
+                <img src="{{ config('app.url') }}/images/icons/staff_icon.png" alt="staff_icon" class="collins-feature-icon">
+                <div class="collins-feature-title">Staff</div>
                 </a>
               </div>
-              <div class="col-md-3">
-                <div class="collins-feature">
-                  <a href="{{ route('schools.edit', $school->id) }}">
-                  <img src="{{ config('app.url') }}/images/icons/school_info_icon.png" alt="schools_info" class="collins-feature-icon">
-                  <div class="collins-feature-title">Terms</div>
-                  </a>
-                </div>
+            </div>
+            <div class="col-md-3">
+              <div class="collins-feature">
+                <a href="{{ route('schools.edit', $school->id) }}">
+                <img src="{{ config('app.url') }}/images/icons/school_info_icon.png" alt="schools_info" class="collins-feature-icon">
+                <div class="collins-feature-title">School Info</div>
+                </a>
               </div>
-              <div class="col-md-3">
-                <div class="collins-feature">
-                  <a href="{{ route('resulttemplates.index') }}">
-                  <img src="{{ config('app.url') }}/images/icons/result_template_icon.png" alt="result_template" class="collins-feature-icon">
-                  <div class="collins-feature-title">Result Templates</div>
-                  </a>
-                </div>
+            </div>
+            <div class="col-md-3">
+              <div class="collins-feature">
+                <a href="{{ route('subscriptions.index') }}">
+                <img src="{{ config('app.url') }}/images/icons/subscription_icon.png" alt="subscription_icon" class="collins-feature-icon">
+                <div class="collins-feature-title">Subscriptions</div>
+                </a>
               </div>
             </div>
           </div>
-          <ul>
-            <li><a href="{{ route('schools.edit', $school->id) }}">School Information</a></li>
-            <li><a href="{{ route('resulttemplates.index') }}">Result templates</a></li>
-            <li><a href="{{ route('directors.index') }}">Directors</a></li>
-            <li><a href="{{ route('subjects.index') }}">Subjects</a></li>
-            <li><a href="{{ route('classes.index') }}">Classes</a></li>
-            <li><a href="{{ route('staff.index') }}">Staff</a></li>
-            <li><a href="{{ route('terms.index') }}">Terms</a></li>
-            <li><a href="#">Report</a></li>
-            <li><a href="{{ route('subscriptions.index') }}">Subscriptions</a></li>
-          </ul>
-          <p>
-            Enjoy!
-          </p>
         </div>
 
       </div>
