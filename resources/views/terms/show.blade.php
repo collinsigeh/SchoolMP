a853ee8353a5c0941dc4114645fb84c930910fbb
Collins Igeh <igehac@gmail.com>
2020-11-06 10:44:38 +0100

    removed the display of classes in the terms.show view and deleted the style

--- resources/views/terms/show.blade.php
+++ resources/views/terms/show.blade.php
@@ -87,19 +87,8 @@
 
                           <div class="row">
                             <div class="col-md-12">
-                                @if (count($term->arms) > 0)
-                                  <div class="row">
-                                    @foreach ($term->arms as $class_arm)
-                                      <div class="col-md-4">
-                                        <a class="btn btn-sm btn-block btn-outline-primary text-left class-arm-display" href="{{ route('arms.show', $class_arm->id) }}">
-                                          <img src="{{ config('app.url') }}/images/icons/classes_icon.png" alt="class" class="options-icon"> {{ $class_arm->schoolclass->name.' '.$class_arm->name }}
-                                        </a>
-                                      </div>
-                                    @endforeach
-                                  </div>
-                                @endif
                                 <div class="table-responsive">
-                                  <table class="table table-striped table-bordered table-hover table-sm">
+                                  <table class="table table-bordered table-hover table-sm">
                                       <tr>
                                         <td><b>No. of class arms:</b> {{ count($term->arms) }}</td>
                                       </tr>
