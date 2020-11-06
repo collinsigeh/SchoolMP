54366e53df32a278c91c090ebf56a11e4a4d1c99
Collins Igeh <igehac@gmail.com>
2020-11-06 11:24:56 +0100

    updated the display of class arm details

--- resources/views/arms/show.blade.php
+++ resources/views/arms/show.blade.php
@@ -51,43 +51,49 @@
         <div class="welcome">
             <div class="row">
                 <div class="col-md-8">
+          
+                    <div class="alert alert-info">
+                      <div style="margin-bottom: 30px;">
+                        <img src="{{ config('app.url') }}/images/icons/classes_icon.png" alt="class_icon" class="collins-this-term-icon"> <span class="collins-this-term">{{ $arm->schoolclass->name.' '.$arm->name }}</span>
+                      </div>
+
+                      <div class="table-responsive">
+                        <table class="table table-striped table-hover table-sm">
+                            <tr>
+                              <th>Class teacher:</th>                           
+                              @if ($arm->user_id < 1)
+                                  <td>
+                                      {!! '<span class="badge badge-danger">No assigned teacher</span>' !!}
+                                      @if ($classarm_manager == 'Yes')
+                                          <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#newTeacherModal">
+                                              Assign class teacher
+                                          </button>
+                                      @endif
+                                  </td>
+                              @else
+                                  <td>{!! $arm->user->name.' - <small>'.$arm->user->staff->phone.'</small>' !!}</td>
+                                  @if ($classarm_manager == 'Yes')
+                                      <td class="text-right">
+                                          <button type="button" class="btn btn-sm btn-outline-primary" data-toggle="modal" data-target="#newTeacherModal">
+                                              Edit
+                                          </button>
+                                      </td>
+                                  @endif
+                              @endif
+                            </tr>
+                        </table>
+                      </div>
+                    </div>
+                    <div style="padding-bottom: 15px;"></div>
+
                     <div class="resource-details">
                         <div class="title">
-                            {{ $arm->schoolclass->name.' '.$arm->name }}
+                            Students
                         </div>
                         <div class="body">
-                            <div class="table-responsive" style="padding-bottom: 68px;">
-                              <table class="table table-striped table-hover table-sm">
-                                  <tr>
-                                    <th>Class teacher:</th>                           
-                                    @if ($arm->user_id < 1)
-                                        <td>
-                                            {!! '<span class="badge badge-danger">No assigned teacher</span>' !!}
-                                            @if ($classarm_manager == 'Yes')
-                                                <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#newTeacherModal">
-                                                    Assign class teacher
-                                                </button>
-                                            @endif
-                                        </td>
-                                    @else
-                                        <td>{!! $arm->user->name.' - <small>'.$arm->user->staff->phone.'</small>' !!}</td>
-                                        @if ($classarm_manager == 'Yes')
-                                            <td class="text-right">
-                                                <button type="button" class="btn btn-sm btn-outline-primary" data-toggle="modal" data-target="#newTeacherModal">
-                                                    Edit
-                                                </button>
-                                            </td>
-                                        @endif
-                                    @endif
-                                  </tr>
-                              </table>
-                            </div>
                             
-                            <div class="table-responsive bg-light">
+                            <div class="table-responsive">
                                 <table class="table table-striped table-hover table-sm">
-                                  <tr class="bg-secondary">
-                                      <th style="font-size: 1.2em; color: #ffffff;" colspan="4">Students</th>
-                                  </tr>
                                   @if (count($arm->enrolments) < 1)
                                       <tr>
                                           <td>None<td>
@@ -128,13 +134,18 @@
                                 </a>
                             </div>
                             @endif
-                            <div style="padding: 35px;"></div>
+                        </div>
+                    </div>
+                    <div style="padding-bottom: 15px;"></div>
+
+                    <div class="resource-details">
+                        <div class="title">
+                            Subjects and assigned teachers
+                        </div>
+                        <div class="body">
                             
                             <div class="table-responsive bg-light">
                                 <table class="table table-striped table-hover table-sm">
-                                  <tr class="bg-secondary">
-                                      <th style="font-size: 1.2em; color: #ffffff;" colspan="5">Subjects and assigned teachers</th>
-                                  </tr>
                                   @if (count($arm->classsubjects) < 1)
                                       <tr>
                                           <td>None<td>
@@ -184,25 +195,36 @@
                                 </button>
                             </div>
                             @endif
-
                         </div>
                     </div>
-                    
                 </div>
-                
+
+
                 <div class="col-md-4">
                     <div class="resource-details">
                         <div class="title">
                             More options
                         </div>
                         <div class="body">
-                            <div class="table-responsive">    
-                              <table class="table">
-                                  <tr>
-                                    <td><a class="btn btn-sm btn-block btn-outline-primary" href="#">Class timetable</a></td>
-                                  </tr>
-                              </table>
-                            </div>
+                          <div class="table-responsive">    
+                            <table class="table">
+                                @if ($classarm_manager == 'Yes')
+                                <tr>
+                                  <td>
+                                    <a class="btn btn-sm btn-block btn-outline-primary text-left" href="{{ route('arms.index') }}">
+                                      <img src="{{ config('app.url') }}/images/icons/classes_icon.png" alt="classes_icon" class="options-icon"> Class arms
+                                    </a>
+                                  </td>
+                                </tr>
+                                @endif
+                                <tr>
+                                  <td>
+                                    <a class="btn btn-sm btn-block btn-outline-primary text-left"  href="{{ route('terms.show', $term->id) }}">
+                                      <img src="{{ config('app.url') }}/images/icons/calendar_icon.png" alt="calendar_icon" class="options-icon"> {!! $term->name.' - <small>'.$term->session.'</small>' !!}</button>
+                                    </td>
+                                </tr>
+                            </table>
+                          </div>
                         </div>
                     </div>
                 </div>
