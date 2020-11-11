8c886cc9ce943b54adc1c4e69a8f01c00efa77cc
Collins Igeh <igehac@gmail.com>
2020-11-11 10:09:49 +0100

    created the modal for adding student scores

---
+++ resources/views/partials/_assign_score.blade.php
@@ -0,0 +1,118 @@
+<!-- assignScoreModal -->
+<div class="modal fade" id="assignScoreModal{{ $result_slip->id }}" tabindex="-1" role="dialog" aria-labelledby="assignScoreModal{{ $result_slip->id }}Label" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="assignScoreModal{{ $result_slip->id }}Label">{!!  '<b>'.$result_slip->enrolment->user->name.'</b><br /><small>('.$result_slip->enrolment->student->registration_number.') ' !!}</small></h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <div class="create-form">
+                <form method="POST" action="{{ route('classsubjects.assign_score', $classsubject->id) }}">
+                    @csrf
+                    @method('PUT')
+
+                    @php
+                        if(isset($returnpage_id) && isset($return_page))
+                        {
+                            echo '<input type="hidden" name="return_page" value="'.$return_page.'" />
+                            <input type="hidden" name="returnpage_id" value="'.$returnpage_id.'" />';
+                        }
+                    @endphp
+    
+                    <div class="form-group row"> 
+                        <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Subject') }}</label>
+    
+                        <div class="col-md-8">
+                            <div class="alert alert-info">
+                                <b>{{ $classsubject->arm->schoolclass->name.' '.$classsubject->arm->name }}</b><br />({!! $term->name.' - <small>'.$term->session.'</small>' !!})
+                            </div>
+                        </div>
+                    </div>
+    
+                    <div class="form-group row"> 
+                        <label for="1st_test_score" class="col-md-4 col-form-label text-md-right">{{ __('1st test score') }}</label>
+    
+                        <div class="col-md-8">
+                            <input id="1st_test_score" type="text" class="form-control @error('1st_test_score') is-invalid @enderror" name="1st_test_score" value="" autocomplete="1st_test_score" autofocus>
+    
+                            @error('1st_test_score')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+    
+                    <div class="form-group row"> 
+                        <label for="2nd_test_score" class="col-md-4 col-form-label text-md-right">{{ __('2nd test score') }}</label>
+    
+                        <div class="col-md-8">
+                            <input id="2nd_test_score" type="text" class="form-control @error('2nd_test_score') is-invalid @enderror" name="2nd_test_score" value="" autocomplete="2nd_test_score" autofocus>
+    
+                            @error('2nd_test_score')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+    
+                    <div class="form-group row"> 
+                        <label for="3rd_test_score" class="col-md-4 col-form-label text-md-right">{{ __('3rd test score') }}</label>
+    
+                        <div class="col-md-8">
+                            <input id="3rd_test_score" type="text" class="form-control @error('3rd_test_score') is-invalid @enderror" name="3rd_test_score" value="" autocomplete="3rd_test_score" autofocus>
+    
+                            @error('3rd_test_score')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+    
+                    <div class="form-group row"> 
+                        <label for="assignment_score" class="col-md-4 col-form-label text-md-right">{{ __('Assignment score') }}</label>
+    
+                        <div class="col-md-8">
+                            <input id="assignment_score" type="text" class="form-control @error('assignment_score') is-invalid @enderror" name="assignment_score" value="" autocomplete="assignment_score" autofocus>
+    
+                            @error('assignment_score')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+    
+                    <div class="form-group row"> 
+                        <label for="exam_score" class="col-md-4 col-form-label text-md-right">{{ __('Exam score') }}</label>
+    
+                        <div class="col-md-8">
+                            <input id="exam_score" type="text" class="form-control @error('exam_score') is-invalid @enderror" name="exam_score" value="" autocomplete="exam_score" autofocus>
+    
+                            @error('exam_score')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+    
+                    <div class="form-group row mb-0">
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Save') }}
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+      </div>
+    </div>
+</div>
+<!-- End assignScoreModal -->
