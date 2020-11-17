7bfe61d291d1222db165a1ff0b3f258a738098f6
Collins Igeh <igehac@gmail.com>
2020-11-17 10:24:39 +0100

    modified the button that displays the payment modal in partials._student_subscription_notice

--- resources/views/partials/_student_subscription_notice.blade.php
+++ resources/views/partials/_student_subscription_notice.blade.php
@@ -1,6 +1,6 @@
 @if ($enrolment->status == 'Inactive')
         <div class="alert alert-danger">
             <h4>Inactive Student Account!</h4>
-            The subscription for this student is <b>inactive</b>. <a href="#" data-toggle="modal" data-target="#makePaymentModal">Make payment</a> to activate.
+            The subscription for this student is <b>inactive</b>. <a href="#" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#makePaymentModal">Make payment</a> to activate.
         </div>
 @endif
