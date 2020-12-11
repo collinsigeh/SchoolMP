42b6471cc7613e343fb18e885831f7150a8efc61
Collins Igeh <igehac@gmail.com>
2020-12-11 11:18:12 +0100

    preparing for results printing

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -20,6 +20,21 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
     @yield('stylesheets')
+    
+    <!-- Script to print the content of a div -->
+    <script> 
+            function printDiv() { 
+                var divContents = document.getElementById("GFG").innerHTML; 
+                var a = window.open('', '', 'height=500, width=500'); 
+                a.document.write('<html>'); 
+                a.document.write('<body > <h1>Div contents are <br>'); 
+                a.document.write(divContents); 
+                a.document.write('</body></html>'); 
+                a.document.close(); 
+                a.print(); 
+            } 
+    </script>
+    <!-- End script to print the content of a div -->
 </head>
 <body>
     <div id="app">
