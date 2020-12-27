04106c533123a8183099776be38681fa1425581b
Collins Igeh <igehac@gmail.com>
2020-12-27 17:54:41 +0100

    modified results.show view - preparing to display the key_to_Grades

--- resources/views/results/show.blade.php
+++ resources/views/results/show.blade.php
@@ -10,7 +10,7 @@
 	<script> 
 		function printDiv() { 
 			var divContents = document.getElementById("GFG").innerHTML; 
-			var a = window.open('', '', 'height=1200, width=900'); 
+			var a = window.open('', '', 'height=1200, width=800'); 
 			a.document.write('<html>'); 
 			a.document.write('<body>'); 
 			a.document.write(divContents); 
@@ -23,7 +23,7 @@
 
 <body> 
 	<center> 
-		<div id="GFG" style="width: 900px;"> 
+		<div id="GFG" style="width: 800px;"> 
 			
 			<div style="border: 3px solid #666; padding: 20px;  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">
 
@@ -31,14 +31,14 @@
 
 					<div style="padding-bottom: 15px; font-size: 0.8em; color: #919191">Termly Result Slip</div>
 
-					<table border="0" cellspacing="0">
+					<table width="100%" border="0" cellspacing="0">
 						<tr style="vertical-align: top;">
-							<td width="98px">
+							<td width="78px">
 
-								<img src="{{ config('app.url') }}/images/school/{{ $enrolment->school->logo }}" alt="School Logo" width="85px;">
+								<img src="{{ config('app.url') }}/images/school/{{ $enrolment->school->logo }}" alt="School Logo" width="75px;">
 
 							</td>
-							<td width="700px" style="text-align: center;">
+							<td style="text-align: center;">
 
 								<div style="font-size: 1.3em; font-weight: 600">
 									{{ strtoupper($enrolment->school->school) }}
@@ -52,7 +52,7 @@
 								</div>
 
 							</td>
-							<td width="98px"></td>
+							<td width="78px"></td>
 						</tr>
 					</table>
 
@@ -60,7 +60,7 @@
 						{{ strtoupper($enrolment->term->session.', '.$enrolment->term->name.' (Termly Examination) Result') }}
 					</div>
 
-					<table cellspacing="0" style="border:1px solid #d3d3d3; text-align: left; font-size: 0.9em;">
+					<table width="100%" cellspacing="0" style="border:1px solid #d3d3d3; text-align: left; font-size: 0.9em;">
 						<tr style="vertical-align: middle;">
 							<td width="73px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600;">
 								Name:
@@ -72,12 +72,12 @@
 							<td width="158px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600;">
 								Class:
 							</td>
-							<td width="225px" style="border:1px solid #d3d3d3; padding: 3px;">
+							<td width="125px" style="border:1px solid #d3d3d3; padding: 3px;">
 								{{ $enrolment->schoolclass->name.' '.$enrolment->arm->name }}
 							</td>
-							<td rowspan="4" width="140px" style="border:1px solid #d3d3d3; padding: 3px; text-align: center">
+							<td rowspan="4" style="border:1px solid #d3d3d3; padding: 3px; text-align: center">
 
-								<img src="{{ config('app.url') }}/images/profile/{{ $enrolment->user->pic }}" alt="Student photo" width="118px;" style="border: 2px solid #d3d3d3; border-radius: 50%;">
+								<img src="{{ config('app.url') }}/images/profile/{{ $enrolment->user->pic }}" alt="Student photo" width="78px;" style="border: 2px solid #d3d3d3; border-radius: 50%;">
 							
 							</td>
 						</tr>
@@ -91,7 +91,7 @@
 							<td width="158px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600;">
 								Reg. no.:
 							</td>
-							<td width="225px" style="border:1px solid #d3d3d3; padding: 3px;">
+							<td width="125px" style="border:1px solid #d3d3d3; padding: 3px;">
 								{{ $enrolment->student->registration_number }}
 							</td>
 						</tr>
@@ -103,9 +103,9 @@
 								{{ date('d-M-Y', strtotime($enrolment->student->date_of_birth)) }}
 							</td>
 							<td width="158px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600;">
-								Term's Enrolment ID:
+								Enrolment ID:
 							</td>
-							<td width="225px" style="border:1px solid #d3d3d3; padding: 3px;">
+							<td width="125px" style="border:1px solid #d3d3d3; padding: 3px;">
 								{{ $enrolment->id }}
 							</td>
 						</tr>
@@ -119,7 +119,7 @@
 							<td width="158px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600;">
 								
 							</td>
-							<td width="225px" style="border:1px solid #d3d3d3; padding: 3px;">
+							<td width="125px" style="border:1px solid #d3d3d3; padding: 3px;">
 								
 							</td>
 						</tr>
@@ -133,28 +133,28 @@
 									$enrolment->arm->resulttemplate->subject_3rd_test_max_score +
 									$enrolment->arm->resulttemplate->subject_assignment_score;
 					?>
-					<table cellspacing="0" style="margin-top: 15px; border:1px solid #d3d3d3; text-align: left; font-size: 0.9em;">
+					<table width="100%" cellspacing="0" style="margin-top: 15px; border:1px solid #d3d3d3; text-align: left; font-size: 0.9em;">
 						<tr style="vertical-align: middle;">
 							<th width="20px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600;">
 								#
 							</th>
-							<th width="315px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600;">
+							<th width="215px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600;">
 								Subject
 							</th>
-							<th width="104px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
+							<th width="74px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
 								C.A. (%)<br />
 								{{ $ca }}
 							</th>
-							<th width="104px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
+							<th width="74px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
 								Exam (%)<br />{{ $enrolment->arm->resulttemplate->subject_exam_score }}
 							</th>
-							<th width="104px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
+							<th width="74px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
 								Total (%)<br />100
 							</th>
 							<th width="55px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
 								Grade
 							</th>
-							<th width="198px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
+							<th style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
 								Remark
 							</th>
 						</tr>
@@ -272,7 +272,7 @@
 						?>
 						<tr style="vertical-align: middle;">
 							<td style="border:1px solid #d3d3d3; padding: 3px;">{{ $sn }}</td>
-							<td style="border:1px solid #d3d3d3; padding: 3px;">{{ $result_slip->classsubject->subject->name }}</td>
+							<td style="border:1px solid #d3d3d3; padding: 3px; font-size: 0.9em;">{{ strtoupper($result_slip->classsubject->subject->name) }}</td>
 							<td style="border:1px solid #d3d3d3; padding: 3px; text-align: center;">{{ $ca }}</td>
 							<td style="border:1px solid #d3d3d3; padding: 3px; text-align: center;">{{ $result_slip->subject_exam_score }}</td>
 							<td style="border:1px solid #d3d3d3; padding: 3px; text-align: center;">{{ $total }}</td>
@@ -285,7 +285,220 @@
 					</table>
 					<?php
 						}
+						else
+						{
+							// for ca_display == 'Breakdown'
 					?>
+					<table width="100%" cellspacing="0" style="margin-top: 15px; border:1px solid #d3d3d3; text-align: left; font-size: 0.9em;">
+						<tr style="vertical-align: middle;">
+							<th width="20px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600;">
+								#
+							</th>
+							<th width="180px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600;">
+								Subject
+							</th>
+							@if ($enrolment->arm->resulttemplate->subject_1st_test_max_score > 0)
+							<th width="54px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
+								1st Test (%)<br />
+								{{ $enrolment->arm->resulttemplate->subject_1st_test_max_score }}
+							</th>
+							@endif
+							@if ($enrolment->arm->resulttemplate->subject_2nd_test_max_score > 0)
+							<th width="54px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
+								2nd Test (%)<br />
+								{{ $enrolment->arm->resulttemplate->subject_2nd_test_max_score }}
+							</th>
+							@endif
+							@if ($enrolment->arm->resulttemplate->subject_3rd_test_max_score > 0)
+							<th width="54px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
+								3rd Test (%)<br />
+								{{ $enrolment->arm->resulttemplate->subject_3rd_test_max_score }}
+							</th>
+							@endif
+							@if ($enrolment->arm->resulttemplate->subject_assignment_score > 0)
+							<th width="54px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
+								Assign. (%)<br />
+								{{ $enrolment->arm->resulttemplate->subject_assignment_score }}
+							</th>
+							@endif
+							<th width="54px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
+								Exam (%)<br />{{ $enrolment->arm->resulttemplate->subject_exam_score }}
+							</th>
+							<th width="54px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
+								Total (%)<br />100
+							</th>
+							<th width="35px" style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
+								Grade
+							</th>
+							<th style="border:1px solid #d3d3d3; padding: 3px; background-color: #e2e2e2; font-size: 0.9em; font-weight: 600; text-align: center;">
+								Remark
+							</th>
+						</tr>
+						<?php
+							$sn = 0;
+							foreach ($enrolment->results as $result_slip) {
+
+								$sn++;
+
+								$total = $result_slip->subject_1st_test_score + $result_slip->subject_2nd_test_score +
+										$result_slip->subject_3rd_test_score + $result_slip->subject_assignment_score +
+										$result_slip->subject_exam_score;
+
+								if($total >= 95 && $total <= 100)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_95_to_100;
+									$remark = $enrolment->arm->resulttemplate->grade_95_to_100;
+								}
+								elseif($total >= 90 && $total <= 94)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_90_to_94;
+									$remark = $enrolment->arm->resulttemplate->grade_90_to_94;
+								}
+								elseif($total >= 85 && $total <= 89)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_85_to_89;
+									$remark = $enrolment->arm->resulttemplate->grade_85_to_89;
+								}
+								elseif($total >= 80 && $total <= 84)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_80_to_84;
+									$remark = $enrolment->arm->resulttemplate->grade_80_to_84;
+								}
+								elseif($total >= 75 && $total <= 79)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_75_to_79;
+									$remark = $enrolment->arm->resulttemplate->grade_75_to_79;
+								}
+								elseif($total >= 70 && $total <= 74)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_70_to_74;
+									$remark = $enrolment->arm->resulttemplate->grade_70_to_74;
+								}
+								elseif($total >= 65 && $total <= 69)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_65_to_69;
+									$remark = $enrolment->arm->resulttemplate->grade_65_to_69;
+								}
+								elseif($total >= 60 && $total <= 64)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_60_to_64;
+									$remark = $enrolment->arm->resulttemplate->grade_60_to_64;
+								}
+								elseif($total >= 55 && $total <= 59)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_55_to_59;
+									$remark = $enrolment->arm->resulttemplate->grade_55_to_59;
+								}
+								elseif($total >= 50 && $total <= 54)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_50_to_54;
+									$remark = $enrolment->arm->resulttemplate->grade_50_to_54;
+								}
+								elseif($total >= 45 && $total <= 49)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_45_to_49;
+									$remark = $enrolment->arm->resulttemplate->grade_45_to_49;
+								}
+								elseif($total >= 40 && $total <= 44)
+								{
+									$grade  = $enrolment->arm->resulttemplate->grade_40_to_44;
+									$remark = $enrolment->arm->resulttemplate->symbol_40_to_44;
+								}
+								elseif($total >= 35 && $total <= 39)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_35_to_39;
+									$remark = $enrolment->arm->resulttemplate->grade_35_to_39;
+								}
+								elseif($total >= 30 && $total <= 34)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_30_to_34;
+									$remark = $enrolment->arm->resulttemplate->grade_30_to_34;
+								}
+								elseif($total >= 25 && $total <= 29)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_25_to_29;
+									$remark = $enrolment->arm->resulttemplate->grade_25_to_29;
+								}
+								elseif($total >= 20 && $total <= 24)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_20_to_24;
+									$remark = $enrolment->arm->resulttemplate->grade_20_to_24;
+								}
+								elseif($total >= 15 && $total <= 19)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_15_to_19;
+									$remark = $enrolment->arm->resulttemplate->grade_15_to_19;
+								}
+								elseif($total >= 10 && $total <= 14)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_10_to_14;
+									$remark = $enrolment->arm->resulttemplate->grade_10_to_14;
+								}
+								elseif($total >= 5 && $total <= 9)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_5_to_9;
+									$remark = $enrolment->arm->resulttemplate->grade_5_to_9;
+								}
+								elseif($total >= 0 && $total <= 4)
+								{
+									$grade  = $enrolment->arm->resulttemplate->symbol_0_to_4;
+									$remark = $enrolment->arm->resulttemplate->grade_0_to_4;
+								}
+						?>
+						<tr style="vertical-align: middle">
+							<td style="border:1px solid #d3d3d3; padding: 3px;">{{ $sn }}</td>
+							<td style="border:1px solid #d3d3d3; padding: 3px; font-size: 0.9em;">{{ strtoupper($result_slip->classsubject->subject->name) }}</td>
+							@if ($enrolment->arm->resulttemplate->subject_1st_test_max_score > 0)
+							<td style="border:1px solid #d3d3d3; padding: 3px; text-align: center;">{{ $result_slip->subject_1st_test_score }}</td>
+							@endif
+							@if ($enrolment->arm->resulttemplate->subject_2nd_test_max_score > 0)
+							<td style="border:1px solid #d3d3d3; padding: 3px; text-align: center;">{{ $result_slip->subject_2nd_test_score }}</td>
+							@endif
+							@if ($enrolment->arm->resulttemplate->subject_3rd_test_max_score > 0)
+							<td style="border:1px solid #d3d3d3; padding: 3px; text-align: center;">{{ $result_slip->subject_3rd_test_score }}</td>
+							@endif
+							@if ($enrolment->arm->resulttemplate->subject_assignment_score > 0)
+							<td style="border:1px solid #d3d3d3; padding: 3px; text-align: center;">{{ $result_slip->subject_assignment_score }}</td>
+							@endif
+							<td style="border:1px solid #d3d3d3; padding: 3px; text-align: center;">{{ $result_slip->subject_exam_score }}</td>
+							<td style="border:1px solid #d3d3d3; padding: 3px; text-align: center;">{{ $total }}</td>
+							<td style="border:1px solid #d3d3d3; padding: 3px; text-align: center;">{{ $grade }}</td>
+							<td style="border:1px solid #d3d3d3; padding: 3px; text-align: center;">{{ $remark }}</td>
+						</tr>
+						<?php
+							}
+						?>
+					</table>
+					<?php
+						}
+					?>
+					
+					<table border="0" cellspacing="0" width="100%">
+						<tr>
+							<td width="250px">
+								<table width="100%" cellspacing="0" style="margin-top: 15px; border:1px solid #d3d3d3; text-align: left; font-size: 0.9em;">
+									<tr style="vertical-align: middle;">
+										<td width="20px" style="border:1px solid #d3d3d3; padding: 3px;">
+											<div style="font-size: 0.9em; font-weight: 600;">Key to Grades</div>
+																					
+										</td>
+									</tr>
+								</table>
+							</td>
+							<td width="10px">
+
+							</td>
+							<td>
+								<table width="100%" cellspacing="0" style="margin-top: 15px; border:1px solid #d3d3d3; text-align: left; font-size: 0.9em;">
+									<tr style="vertical-align: middle;">
+										<td width="20px" style="border:1px solid #d3d3d3; padding: 3px;">
+											<div style="font-size: 0.9em; font-weight: 600; text-align: center;">Overall Performance</div>											
+										</td>
+									</tr>
+								</table>
+							</td>
+						</tr>
+					</table>
 
 				</div> 
 			
